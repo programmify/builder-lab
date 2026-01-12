71bdb5c115d9e3ff20cfaac520a1ffee63e4d62a
tonycletus <anthonyomojocletus@gmail.com>
2026-01-12 03:09:04 +0100

    fix vercel analytic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,8 +30,8 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
+          <Analytics />
         </BrowserRouter>
-        <Analytics />
       </div>
     </TooltipProvider>
   </QueryClientProvider>
